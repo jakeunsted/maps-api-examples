9a113b8ed305be322933cbcec27759a47905fbd6
jakeunsted <jake.unsted@gmail.com>
2024-08-28 17:32:06 +0100

    labeled pins

--- src/components/maps/google/StaticMapPinDrop.vue
+++ src/components/maps/google/StaticMapPinDrop.vue
@@ -9,17 +9,16 @@ const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
 
 const center = '50.861658,-0.083969' // Center of the map (latitude, longitude)
 const zoom = 10 // Zoom level
-const size = '800x400' // Size of the map image (width x height)
-const markers = [
-  '50.861658,-0.083969', // Marker 1
-  '50.821483,-0.096042', // Marker 2
-  '50.820338,-0.384817' // Marker 3
-]
+const size = '600x400' // Size of the map image (width x height)
 const altText = 'Static map with multiple markers'
 
-// Construct the markers parameter for the URL
-const markersParam = markers.map((marker) => `markers=color:red%7C${marker}`).join('&')
+const markers = [
+  'color:blue|label:A|50.861658,-0.083969', // Marker A with blue color
+  'color:green|label:B|50.821483,-0.096042', // Marker B with green color
+  'color:red|label:C|50.820338,-0.384817' // Marker C with red color
+]
 
-// Generate the Google Maps Static API URL
+const markersParam = markers.map((marker) => `markers=${encodeURIComponent(marker)}`).join('&')
+
 const mapUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${center}&zoom=${zoom}&size=${size}&${markersParam}&key=${apiKey}`
 </script>
